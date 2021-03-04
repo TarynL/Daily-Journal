2cb058367e62230d7d0cde915643f3ffe307527d
Taryn <Taryn@Flyings-MacBook-Pro.local>
2021-03-03 19:23:27 -0600

    added JS

---
+++ scripts/JournalData.js
@@ -0,0 +1,53 @@
+/*
+ *   Journal data for Daily Journal application
+ *
+ *      Holds the raw data about each entry and exports
+ *      functions that other modules can use to filter
+ *      the entries for different purposes.
+ */
+
+// This is the original data.
+const journal = [
+    {
+        id: 1,
+        date: "07/24/2025",
+        concept: "HTML & CSS",
+        entry: "We talked about HTML components and how to make grid layouts with Flexbox in CSS.",
+        mood: "happy"
+    },
+    {
+        id: 2,
+        date: "08/09/1991",
+        concept: "Flexbox",
+        entry: "Learned how flexbox makes your css life easier yet more complicated.",
+        mood: "mad"
+    },
+    {
+        id: 4,
+        date: "07/24/2025",
+        concept: "Automated",
+        entry: "Lets make data loop through to make our lives a less stressful",
+        mood: "content"
+    },
+    {
+        id: 3,
+        date: "07/24/2025",
+        concept: "Javascript",
+        entry: "Javascript on javascript, imported and exported, then lets fix all the errors and be done.",
+        mood: "sad"
+    }
+    
+
+]
+
+/*
+    You export a function that provides a version of the
+    raw data in the format that you want
+*/
+export const getJournalEntries = () => {
+    const sortedByDate = journal.sort(
+        (currentEntry, nextEntry) =>
+            Date.parse(currentEntry.date) - Date.parse(nextEntry.date)
+    )
+    return sortedByDate
+}
