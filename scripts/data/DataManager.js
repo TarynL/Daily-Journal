297699ecbbaa7b9ce8e46e92415a8b5fb86f4352
Taryn <Taryn@Flyings-MacBook-Pro.local>
2021-03-05 14:14:09 -0600

    addedDataManager.js

--- scripts/data/DataManager.js
+++ scripts/data/DataManager.js
@@ -1,9 +1,25 @@
 export const getJournalEntries = () => {
 
-    return fetch("http://localhost:8088/journalEntry")
-    .then(response => response.json())
-    .then(parsedResponse => {
-        // do something with response here
-        return parsedResponse;
-    })
-}
+    return fetch("http://localhost:8088/JournalEntry")
+        .then(response => response.json())
+        
+};
+
+export const getEntryTopic = () => {
+
+    return fetch("http://localhost:8088/EntryTopic")
+        .then(response => response.json())
+        
+};
+
+const defaultEntry = {
+    id: 1,
+    date: "03/05/2021",
+    concept: "GitHub",
+    entry: "Learned how to add & commit to then push repo up.",
+    mood: "happy"
+};
+
+export const getJournalEntries = () => {
+    return defaultEntry;
+};
