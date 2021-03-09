5c89313a68d9eed2466328d0cf88ab62c0c11841
Taryn <Taryn@Flyings-MacBook-Pro.local>
2021-03-09 17:01:01 -0600

    in the process of adding event selectors to entries

--- scripts/data/Entry.js
+++ scripts/data/Entry.js
@@ -5,11 +5,11 @@ export const Entry = (entryObj) => {
         
         <h4 class ="entryTopic">#${entryObj.id}   ${entryObj.concept}</h4>
 
-        <div>${entryObj.date}</div>
+        <div class ="entryDate">${entryObj.date}</div>
 
         <div>${entryObj.entry}</div>
 
-        <div>How I was feeling:  ${entryObj.mood}</div>
+        <div class ="pastMood">How I was feeling:  ${entryObj.mood}</div>
         </section>
     `
 }
